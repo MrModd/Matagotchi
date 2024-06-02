flipperdevices/flipperzero-firmware

Refactor features to ease the addition of new ones
Defines an interface for all features and maintain an array of features
to update the game state regardless of how many there are.

src/feature_management.h
@@ -5,45 +5,38 @@
 
 #include "game_structs.h"
 
-/* Every feature has to implement 4 methods:
+/* Every feature has to implement a function returning the following Feat struct.
+ * After defining it, you need to call that function to initialize a corresponding entry
+ * in the features array in state_management.c, within the init_state function.
  *
- * void init_<FEATURE>(struct GameState *game_state, uint32_t current_timestamp)
+ * The purpose of the methods of that struct are described here:
+ *
+ * void init(struct GameState *game_state, uint32_t current_timestamp)
  *   Initializes the feature variables inside the @game_state.
  *   The feature may needs the last update of the params, it can use @current_timestamp
  *   as init value.
  *
- * void check_<FEATURE>(const struct GameState *game_state, uint32_t current_timestamp, struct GameEvents *game_events)
+ * void check(const struct GameState *game_state, uint32_t current_timestamp, struct GameEvents *game_events)
  *   Verifies if at @current_timestamp there are updates to do to the params of the feature.
  *   It should use the @game_state only as reference. It should update @game_events instead
  *   to notify eventual updates to apply.
  *
- * bool apply_<FEATURE>(struct GameState *game_state, struct GameEvents game_events)
+ * bool apply(struct GameState *game_state, struct GameEvents game_events)
  *   Applies the updates calculated by the previous function and passed here by @game_events
  *   to the @game_state.
  *   @return: true if changes have been made to @game_state, false otherwise
  *
- * int get_text_<FEATURE>(const struct GameState *game_state, char *str, size_t size)
+ * int get_text(const struct GameState *game_state, char *str, size_t size)
  *   Write in @str the text that represent the status of this feature.
  *   @return: the number of character written excluding the trailing \0
  */
 
-// Experience
-void init_xp(struct GameState *, uint32_t);
-void check_xp(const struct GameState *, uint32_t, struct GameEvents *);
-bool apply_xp(struct GameState *, struct GameEvents);
-int get_text_xp(const struct GameState *, char *, size_t);
-
-// Hunger
-void init_hu(struct GameState *, uint32_t);
-void check_hu(const struct GameState *, uint32_t, struct GameEvents *);
-bool apply_hu(struct GameState *, struct GameEvents);
-int get_text_hu(const struct GameState *, char *, size_t);
-
-// Health (depends on HU)
-void init_hp(struct GameState *, uint32_t);
-void check_hp(const struct GameState *, uint32_t, struct GameEvents *);
-bool apply_hp(struct GameState *, struct GameEvents);
-int get_text_hp(const struct GameState *, char *, size_t);
+struct Feat {
+	void (*init) (struct GameState *, uint32_t);
+	void (*check) (const struct GameState *, uint32_t, struct GameEvents *);
+	bool (*apply) (struct GameState *, struct GameEvents);
+	int (*get_text) (const struct GameState *, char *, size_t);
+};
 
 /* correct_state
  * It is use to verify that the updates

src/features/experience.c
@@ -1,0 +1,87 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "experience.h"
+#include "../settings_management.h"
+#include "../constants.h"
+#include "../random_generator.h"
+
+static void init(struct GameState *game_state, uint32_t current_timestamp) {
+    game_state->persistent.xp = 0;
+    game_state->persistent.last_recorded_xp_update = current_timestamp;
+}
+
+static void check(const struct GameState *game_state, uint32_t current_timestamp, struct GameEvents *game_events) {
+    uint32_t last_timestamp = game_state->persistent.last_recorded_xp_update;
+    uint32_t nb_events = (current_timestamp - last_timestamp) / NEW_XP_FREQUENCY;
+
+    FURI_LOG_D(LOG_TAG, "check_xp(): current_timestamp=%lu; last_timestamp=%lu; nb_events=%lu",
+        current_timestamp,
+        last_timestamp,
+        nb_events);
+
+    // If some events are extracted, the timestamp will be updated
+    // even though there are no XP to add.
+    game_events->xp_timestamp = (nb_events) ? current_timestamp : last_timestamp;
+
+    while(nb_events-- > 0) {
+        if (toss_a_coin(NEW_XP_PROBABILITY)) {
+            game_events->xp++;
+        }
+    }
+
+    if (game_events->xp) {
+        FURI_LOG_I(LOG_TAG, "Gained %lu new XP!", game_events->xp);
+    }
+}
+
+static bool apply(struct GameState *game_state, struct GameEvents game_events) {
+    bool state_updated = false;
+
+    if (game_events.xp_timestamp == 0) {
+        // XP events not generated
+        return false;
+    }
+
+    while(game_events.xp > 0 && game_state->persistent.stage != DEAD) {
+        state_updated = true;
+        uint32_t max_xp_this_stage = MAX_XP_PER_STAGE[game_state->persistent.stage];
+
+        if (game_state->persistent.xp + game_events.xp >= max_xp_this_stage) {
+            FURI_LOG_D(LOG_TAG, "apply_xp(): need to apply %lu new XP, can only %lu "
+                "in current stage %u (%lu XP already present in current stage)",
+                game_events.xp,
+                max_xp_this_stage - game_state->persistent.xp,
+                game_state->persistent.stage,
+                game_state->persistent.xp);
+
+            game_events.xp -= max_xp_this_stage - game_state->persistent.xp;
+            game_state->persistent.xp = 0;
+            game_state->persistent.stage++;
+            FURI_LOG_I(LOG_TAG, "Evoluted to new stage %u!", game_state->persistent.stage);
+            play_level_up(game_state);
+            vibrate_long(game_state);
+        } else {
+            game_state->persistent.xp += game_events.xp;
+            game_events.xp = 0;
+            FURI_LOG_D(LOG_TAG, "apply_xp(): new total is %lu XP", game_state->persistent.xp);
+        }
+    }
+    game_state->persistent.last_recorded_xp_update = game_events.xp_timestamp;
+    return state_updated;
+}
+
+static int get_text(const struct GameState *game_state, char *str, size_t size) {
+    return snprintf(str, size, "XP: %lu/%lu\n",
+                    game_state->persistent.xp,
+                    MAX_XP_PER_STAGE[game_state->persistent.stage]);
+}
+
+struct Feat init_feat_experience() {
+	struct Feat feat;
+	feat.init = init;
+	feat.check = check;
+	feat.apply = apply;
+	feat.get_text = get_text;
+	return feat;
+}

src/features/hunger.c
@@ -1,0 +1,90 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "hunger.h"
+#include "../settings_management.h"
+#include "../constants.h"
+#include "../random_generator.h"
+
+static void init(struct GameState *game_state, uint32_t current_timestamp) {
+    game_state->persistent.hu = MAX_HU;
+    game_state->persistent.last_recorded_hu_update = current_timestamp;
+}
+
+static void check(const struct GameState *game_state, uint32_t current_timestamp, struct GameEvents *game_events) {
+    uint32_t last_timestamp = game_state->persistent.last_recorded_hu_update;
+    uint32_t nb_events = (current_timestamp - last_timestamp) / LOSE_HU_FREQUENCY;
+
+    FURI_LOG_D(LOG_TAG, "check_hu(): current_timestamp=%lu; last_timestamp=%lu; nb_events=%lu",
+        current_timestamp,
+        last_timestamp,
+        nb_events);
+
+    // If some events are extracted, the timestamp will be updated
+    // even though there are no HP to add.
+    game_events->hu_timestamp = (nb_events) ? current_timestamp : last_timestamp;
+
+    while(nb_events-- > 0) {
+        if (toss_a_coin(LOSE_HU_PROBABILITY)) {
+            game_events->hu -= random_uniform(LOSE_HU_MIN, LOSE_HU_MAX);
+        }
+    }
+
+    if (game_events->hu) {
+        FURI_LOG_I(LOG_TAG, "Lost %ld HU!", -(game_events->hu));
+    }
+}
+
+static bool apply(struct GameState *game_state, struct GameEvents game_events) {
+    int32_t hu = game_events.hu;
+
+    if (game_events.hu_timestamp == 0) {
+        // HU events not generated
+        return false;
+    }
+
+    if (hu < 0) {
+        uint32_t lost_hu = (uint32_t)-hu;
+        // Lost some HU
+        if (game_state->persistent.hu > lost_hu) {
+            // There are still HU left
+            game_state->persistent.hu -= lost_hu;
+        } else {
+            // Started to starve
+            game_state->persistent.hu = 0;
+            FURI_LOG_I(LOG_TAG, "The pet is hungry!");
+            play_starvation(game_state);
+            vibrate_long(game_state);
+        }
+    } else if (hu > 0) {
+        // Ate some food
+        if (game_state->persistent.hu + hu > MAX_HU) {
+            // Gained more than max HU
+            game_state->persistent.hu = MAX_HU;
+        } else {
+            game_state->persistent.hu += hu;
+        }
+    }
+
+    game_state->persistent.last_recorded_hu_update = game_events.hu_timestamp;
+    if (hu != 0) {
+        FURI_LOG_D(LOG_TAG, "apply_hu(): new total is %lu HU", game_state->persistent.hu);
+        return true;
+    }
+    return false;
+}
+
+static int get_text(const struct GameState *game_state, char *str, size_t size) {
+    return snprintf(str, size, "HU: %lu/%d\n",
+                    game_state->persistent.hu,
+                    MAX_HU);
+}
+
+struct Feat init_feat_hunger() {
+	struct Feat feat;
+	feat.init = init;
+	feat.check = check;
+	feat.apply = apply;
+	feat.get_text = get_text;
+	return feat;
+}

src/state_management.c
@@ -4,9 +4,15 @@
 #include "state_management.h"
 #include "constants.h"
 #include "feature_management.h"
+#include "features/experience.h"
+#include "features/hunger.h"
+#include "features/health.h"
 #include "settings_management.h"
 #include "save_restore.h"
 #include "game_structs.h"
+
+#define NUM_FEAT 3
+static struct Feat features[NUM_FEAT];
 
 static uint32_t get_current_timestamp() {
     FuriHalRtcDateTime current_time;
@@ -28,12 +34,16 @@ static void init_persistent_state_object(struct GameState *game_state) {
     game_state->persistent.stage = EGG;
 
     // Init every individual feature
-    init_xp(game_state, current_timestamp);
-    init_hu(game_state, current_timestamp);
-    init_hp(game_state, current_timestamp);
+		for(uint32_t i = 0; i < NUM_FEAT; ++i) {
+			features[i].init(game_state, current_timestamp);
+		}
 }
 
 void init_state(struct GameState *game_state) {
+		features[0] = init_feat_experience();
+		features[1] = init_feat_hunger();
+		features[2] = init_feat_health();
+
     // Try to load the state from the storage
     if (!load_state_from_file(&game_state->persistent)) {
         init_persistent_state_object(game_state);
@@ -64,9 +74,9 @@ static void _generate_new_random_event(uint32_t timestamp, struct GameState *gam
         return;
     }
     // Check every individual feature
-    check_xp(game_state, timestamp, game_events);
-    check_hu(game_state, timestamp, game_events);
-    check_hp(game_state, timestamp, game_events);
+		for(uint32_t i = 0; i < NUM_FEAT; ++i) {
+			features[i].check(game_state, timestamp, game_events);
+		}
 }
 
 void generate_new_random_events(struct GameState *game_state, struct GameEvents *game_events) {
@@ -79,9 +89,9 @@ bool process_events(struct GameState *game_state, struct GameEvents game_events)
     bool new_events = false;
 
     // Process every individual feature
-    new_events |= apply_xp(game_state, game_events);
-    new_events |= apply_hu(game_state, game_events);
-    new_events |= apply_hp(game_state, game_events);
+		for(uint32_t i = 0; i < NUM_FEAT; ++i) {
+			new_events |= features[i].apply(game_state, game_events);
+		}
 
     if (new_events) {
         correct_state(game_state);
@@ -97,21 +107,11 @@ void get_state_str(const struct GameState *game_state, char *str, size_t size) {
                       LIFE_STAGE_STRING[game_state->persistent.stage]);
 
     // Append every individual feature
-    str += copied;
-    size -= copied;
-    copied = get_text_xp(game_state, str, size);
-    str += copied;
-    size -= copied;
-    copied = snprintf(str, size, "\n");
-    str += copied;
-    size -= copied;
-    copied = get_text_hu(game_state, str, size);
-    str += copied;
-    size -= copied;
-    copied = snprintf(str, size, "\n");
-    str += copied;
-    size -= copied;
-    copied = get_text_hp(game_state, str, size);
+		for(uint32_t i = 0; i < NUM_FEAT; ++i) {
+			str += copied;
+			size -= copied;
+			copied = features[i].get_text(game_state, str, size);
+		}
 }
 
 void give_candy(struct GameState *game_state, struct GameEvents *game_events) {

MrModd/Matagotchi

src/feature_management.h
@@ -6,45 +6,38 @@
 #include "flipper_structs.h"
 #include "game_structs.h"
 
-/* Every feature has to implement 4 methods:
+/* Every feature has to implement a function returning the following Feat struct.
+ * After defining it, you need to call that function to initialize a corresponding entry
+ * in the features array in state_management.c, within the init_state function.
  *
- * void init_<FEATURE>(struct GameState *game_state, uint32_t current_timestamp)
+ * The purpose of the methods of that struct are described here:
+ *
+ * void init(struct GameState *game_state, uint32_t current_timestamp)
  *   Initializes the feature variables inside the @game_state.
  *   The feature may needs the last update of the params, it can use @current_timestamp
  *   as init value.
  *
- * void check_<FEATURE>(const struct ApplicationContext *context, uint32_t current_timestamp, struct GameEvents *game_events)
+ * void check(const struct ApplicationContext *context, uint32_t current_timestamp, struct GameEvents *game_events)
  *   Verifies if at @current_timestamp there are updates to do to the params of the feature.
  *   It should use the @game_state only as reference. It should update @game_events instead
  *   to notify eventual updates to apply.
  *
- * bool apply_<FEATURE>(struct ApplicationContext *context, struct GameEvents game_events)
+ * bool apply(struct ApplicationContext *context, struct GameEvents game_events)
  *   Applies the updates calculated by the previous function and passed here by @game_events
  *   to the @game_state.
  *   @return: true if changes have been made to @game_state, false otherwise
  *
- * int get_text_<FEATURE>(const struct GameState *game_state, char *str, size_t size)
+ * int get_text(const struct GameState *game_state, char *str, size_t size)
  *   Write in @str the text that represent the status of this feature.
  *   @return: the number of character written excluding the trailing \0
  */
 
-// Experience
-void init_xp(struct GameState *, uint32_t);
-void check_xp(const struct ApplicationContext *, uint32_t, struct GameEvents *);
-bool apply_xp(struct ApplicationContext *, struct GameEvents);
-int get_text_xp(const struct GameState *, char *, size_t);
-
-// Hunger
-void init_hu(struct GameState *, uint32_t);
-void check_hu(const struct ApplicationContext *, uint32_t, struct GameEvents *);
-bool apply_hu(struct ApplicationContext *, struct GameEvents);
-int get_text_hu(const struct GameState *, char *, size_t);
-
-// Health (depends on HU)
-void init_hp(struct GameState *, uint32_t);
-void check_hp(const struct ApplicationContext *, uint32_t, struct GameEvents *);
-bool apply_hp(struct ApplicationContext *, struct GameEvents);
-int get_text_hp(const struct GameState *, char *, size_t);
+struct Feat {
+    void (*init) (struct GameState *, uint32_t);
+    void (*check) (const struct ApplicationContext *, uint32_t, struct GameEvents *);
+    bool (*apply) (struct ApplicationContext *, struct GameEvents);
+    int (*get_text) (const struct GameState *, char *, size_t);
+};
 
 /* correct_state
  * It is use to verify that the updates

src/features/experience.c
@@ -1,0 +1,89 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "experience.h"
+#include "../settings_management.h"
+#include "../constants.h"
+#include "../random_generator.h"
+
+static void init(struct GameState *game_state, uint32_t current_timestamp) {
+    game_state->persistent.xp = 0;
+    game_state->persistent.last_recorded_xp_update = current_timestamp;
+}
+
+static void check(const struct ApplicationContext *context, uint32_t current_timestamp, struct GameEvents *game_events) {
+		struct GameState *game_state = context->game_state;
+    uint32_t last_timestamp = game_state->persistent.last_recorded_xp_update;
+    uint32_t nb_events = (current_timestamp - last_timestamp) / NEW_XP_FREQUENCY;
+
+    FURI_LOG_D(LOG_TAG, "check_xp(): current_timestamp=%lu; last_timestamp=%lu; nb_events=%lu",
+        current_timestamp,
+        last_timestamp,
+        nb_events);
+
+    // If some events are extracted, the timestamp will be updated
+    // even though there are no XP to add.
+    game_events->xp_timestamp = (nb_events) ? current_timestamp : last_timestamp;
+
+    while(nb_events-- > 0) {
+        if (toss_a_coin(NEW_XP_PROBABILITY)) {
+            game_events->xp++;
+        }
+    }
+
+    if (game_events->xp) {
+        FURI_LOG_I(LOG_TAG, "Gained %lu new XP!", game_events->xp);
+    }
+}
+
+static bool apply(struct ApplicationContext *context, struct GameEvents game_events) {
+		struct GameState *game_state = context->game_state;
+    bool state_updated = false;
+
+    if (game_events.xp_timestamp == 0) {
+        // XP events not generated
+        return false;
+    }
+
+    while(game_events.xp > 0 && game_state->persistent.stage != DEAD) {
+        state_updated = true;
+        uint32_t max_xp_this_stage = MAX_XP_PER_STAGE[game_state->persistent.stage];
+
+        if (game_state->persistent.xp + game_events.xp >= max_xp_this_stage) {
+            FURI_LOG_D(LOG_TAG, "apply_xp(): need to apply %lu new XP, can only %lu "
+                "in current stage %u (%lu XP already present in current stage)",
+                game_events.xp,
+                max_xp_this_stage - game_state->persistent.xp,
+                game_state->persistent.stage,
+                game_state->persistent.xp);
+
+            game_events.xp -= max_xp_this_stage - game_state->persistent.xp;
+            game_state->persistent.xp = 0;
+            game_state->persistent.stage++;
+            FURI_LOG_I(LOG_TAG, "Evoluted to new stage %u!", game_state->persistent.stage);
+            play_level_up(game_state);
+            vibrate_long(context);
+        } else {
+            game_state->persistent.xp += game_events.xp;
+            game_events.xp = 0;
+            FURI_LOG_D(LOG_TAG, "apply_xp(): new total is %lu XP", game_state->persistent.xp);
+        }
+    }
+    game_state->persistent.last_recorded_xp_update = game_events.xp_timestamp;
+    return state_updated;
+}
+
+static int get_text(const struct GameState *game_state, char *str, size_t size) {
+    return snprintf(str, size, "XP: %lu/%lu\n",
+                    game_state->persistent.xp,
+                    MAX_XP_PER_STAGE[game_state->persistent.stage]);
+}
+
+struct Feat init_feat_experience() {
+    struct Feat feat;
+    feat.init = init;
+    feat.check = check;
+    feat.apply = apply;
+    feat.get_text = get_text;
+    return feat;
+}

src/features/hunger.c
@@ -1,0 +1,92 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "hunger.h"
+#include "../settings_management.h"
+#include "../constants.h"
+#include "../random_generator.h"
+
+static void init(struct GameState *game_state, uint32_t current_timestamp) {
+    game_state->persistent.hu = MAX_HU;
+    game_state->persistent.last_recorded_hu_update = current_timestamp;
+}
+
+static void check(const struct ApplicationContext *context, uint32_t current_timestamp, struct GameEvents *game_events) {
+		struct GameState *game_state = context->game_state;
+    uint32_t last_timestamp = game_state->persistent.last_recorded_hu_update;
+    uint32_t nb_events = (current_timestamp - last_timestamp) / LOSE_HU_FREQUENCY;
+
+    FURI_LOG_D(LOG_TAG, "check_hu(): current_timestamp=%lu; last_timestamp=%lu; nb_events=%lu",
+        current_timestamp,
+        last_timestamp,
+        nb_events);
+
+    // If some events are extracted, the timestamp will be updated
+    // even though there are no HP to add.
+    game_events->hu_timestamp = (nb_events) ? current_timestamp : last_timestamp;
+
+    while(nb_events-- > 0) {
+        if (toss_a_coin(LOSE_HU_PROBABILITY)) {
+            game_events->hu -= random_uniform(LOSE_HU_MIN, LOSE_HU_MAX);
+        }
+    }
+
+    if (game_events->hu) {
+        FURI_LOG_I(LOG_TAG, "Lost %ld HU!", -(game_events->hu));
+    }
+}
+
+static bool apply(struct ApplicationContext *context, struct GameEvents game_events) {
+		struct GameState *game_state = context->game_state;
+    int32_t hu = game_events.hu;
+
+    if (game_events.hu_timestamp == 0) {
+        // HU events not generated
+        return false;
+    }
+
+    if (hu < 0) {
+        uint32_t lost_hu = (uint32_t)-hu;
+        // Lost some HU
+        if (game_state->persistent.hu > lost_hu) {
+            // There are still HU left
+            game_state->persistent.hu -= lost_hu;
+        } else {
+            // Started to starve
+            game_state->persistent.hu = 0;
+            FURI_LOG_I(LOG_TAG, "The pet is hungry!");
+            play_starvation(game_state);
+            vibrate_long(context);
+        }
+    } else if (hu > 0) {
+        // Ate some food
+        if (game_state->persistent.hu + hu > MAX_HU) {
+            // Gained more than max HU
+            game_state->persistent.hu = MAX_HU;
+        } else {
+            game_state->persistent.hu += hu;
+        }
+    }
+
+    game_state->persistent.last_recorded_hu_update = game_events.hu_timestamp;
+    if (hu != 0) {
+        FURI_LOG_D(LOG_TAG, "apply_hu(): new total is %lu HU", game_state->persistent.hu);
+        return true;
+    }
+    return false;
+}
+
+static int get_text(const struct GameState *game_state, char *str, size_t size) {
+    return snprintf(str, size, "HU: %lu/%d\n",
+                    game_state->persistent.hu,
+                    MAX_HU);
+}
+
+struct Feat init_feat_hunger() {
+    struct Feat feat;
+    feat.init = init;
+    feat.check = check;
+    feat.apply = apply;
+    feat.get_text = get_text;
+    return feat;
+}

src/state_management.c
@@ -4,9 +4,15 @@
 #include "state_management.h"
 #include "constants.h"
 #include "feature_management.h"
+#include "features/experience.h"
+#include "features/hunger.h"
+#include "features/health.h"
 #include "settings_management.h"
 #include "save_restore.h"
 #include "game_structs.h"
+
+#define NUM_FEAT 3
+static struct Feat features[NUM_FEAT];
 
 static uint32_t get_current_timestamp() {
     DateTime current_time;
@@ -30,13 +36,16 @@ static void init_persistent_state_object(struct GameState *game_state) {
     game_state->persistent.stage = EGG;
 
     // Init every individual feature
-    init_xp(game_state, current_timestamp);
-    init_hu(game_state, current_timestamp);
-    init_hp(game_state, current_timestamp);
+    for(uint32_t i = 0; i < NUM_FEAT; ++i) {
+        features[i].init(game_state, current_timestamp);
+    }
 }
 
 void init_state(struct ApplicationContext *context) {
     struct GameState *game_state = context->game_state;
+    features[0] = init_feat_experience();
+    features[1] = init_feat_hunger();
+    features[2] = init_feat_health();
 
     // Try to load the state from the storage
     if (!load_state_from_file(&game_state->persistent)) {
@@ -68,9 +77,9 @@ static void _generate_new_random_event(uint32_t timestamp, struct ApplicationCon
         return;
     }
     // Check every individual feature
-    check_xp(context, timestamp, game_events);
-    check_hu(context, timestamp, game_events);
-    check_hp(context, timestamp, game_events);
+    for(uint32_t i = 0; i < NUM_FEAT; ++i) {
+        features[i].check(context, timestamp, game_events);
+    }
 }
 
 void generate_new_random_events(struct ApplicationContext *context, struct GameEvents *game_events) {
@@ -83,9 +92,9 @@ bool process_events(struct ApplicationContext *context, struct GameEvents game_e
     bool new_events = false;
 
     // Process every individual feature
-    new_events |= apply_xp(context, game_events);
-    new_events |= apply_hu(context, game_events);
-    new_events |= apply_hp(context, game_events);
+    for(uint32_t i = 0; i < NUM_FEAT; ++i) {
+        new_events |= features[i].apply(context, game_events);
+    }
 
     if (new_events) {
         correct_state(context->game_state);
@@ -101,21 +110,11 @@ void get_state_str(const struct GameState *game_state, char *str, size_t size) {
                       LIFE_STAGE_STRING[game_state->persistent.stage]);
 
     // Append every individual feature
-    str += copied;
-    size -= copied;
-    copied = get_text_xp(game_state, str, size);
-    str += copied;
-    size -= copied;
-    copied = snprintf(str, size, "\n");
-    str += copied;
-    size -= copied;
-    copied = get_text_hu(game_state, str, size);
-    str += copied;
-    size -= copied;
-    copied = snprintf(str, size, "\n");
-    str += copied;
-    size -= copied;
-    copied = get_text_hp(game_state, str, size);
+    for(uint32_t i = 0; i < NUM_FEAT; ++i) {
+        str += copied;
+        size -= copied;
+        copied = features[i].get_text(game_state, str, size);
+    }
 }
 
 void give_candy(struct GameState *game_state, struct GameEvents *game_events) {